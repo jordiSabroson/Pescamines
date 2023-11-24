834f07a2821cec8f667813c91c1da6101c7de255
Jordi Sabroson <2023_jordi.manya@iticbcn.cat>
2023-11-24 17:15:38 +0100

    avanç

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let files = 0, columnes = 0;
 let taula = "<table>";
-let percentatge;
+let percentatge, total;
+let aleatori;
+let minesArray;
+
 function iniciarPartida() {
-    files = prompt("Introdueix un número de columnes per fer la taula (min 10 i max 30)");
+    files = prompt("Introdueix un número de files per fer la taula (min 10 i max 30)");
     columnes = prompt("Ara un número de columnes (min 10 i max 30)");
 
     if (files < 10) {
@@ -23,23 +26,37 @@ function iniciarPartida() {
 
 function crearTaulell(columnes, files) {
 
-    percentatge = ((columnes / files * 100) / 17).toFixed(0);
-
-    alert(percentatge);
     for (let i = 0; i < files; i++) {
         taula += "<tr>";
         for (let a = 0; a < columnes; a++) {
-            taula += "<td onclick='obreCasella()'><img src='img/fons.jpg'></td>"
+            taula += "<td id='" + i + "," + a +"' onclick='obreCasella(" + i + "," + a +")' data-mina='false'><img src='img/fons.jpg'></td>"
         }
         taula += "</tr>";
     }
 
     taula += "</table>";
-    
+
     document.getElementById("taulell").innerHTML = taula;
+
+    setMines();
 }
 
-function obreCasella() {
+function setMines() {
+    total = columnes * files;
+    percentatge = Math.floor(total * 0.17);
+
+    calculaAdjacents();
+}
+
+function obreCasella(x, y) {
+    let casella = document.getElementById(x, y);
+    let teMina = casella.getAttribute("data-mina") === "true";
+
+    teMina ? casella.innerHTML = "<img src='img/mina.jpg'></img>" : casella.innerHTML = "1";
+}
+
+
+function calculaAdjacents() {
 
 }
 
